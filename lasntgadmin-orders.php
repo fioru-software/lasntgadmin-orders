636bbbfb11365e8e360f8ff28d97d102939d0dcf
cm <7zh6dj9f9@mozmail.com>
2022-11-21 16:52:51 +0000

    depends on other plugins

--- lasntgadmin-orders.php
+++ lasntgadmin-orders.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name:       Example Plugin
- * Plugin URI:        https://github.com/fioru-software/lasntg-plugin_template
+ * Plugin Name:       LASNTG Orders Plugin
+ * Plugin URI:        https://github.com/fioru-software/lasntgadmin-orders
  * Description:       An example plugin.
- * Version:           0.0.0
+ * Version:           0.0.1
  * Requires PHP:      7.2
- * Text Domain:       my-basics-plugin
+ * Text Domain:       lasntgadmin
  * Domain Path:       /languages
  */
 
