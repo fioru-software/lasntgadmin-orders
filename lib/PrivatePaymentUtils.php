d4ae6e55f54431bb9276ff1f5af358f81c60567d
cm <7zh6dj9f9@mozmail.com>
2023-04-18 14:27:30 +0100

    show products to guests in shop

--- lib/PrivatePaymentUtils.php
+++ lib/PrivatePaymentUtils.php
@@ -2,6 +2,9 @@
 
 namespace Lasntg\Admin\Orders;
 
+use Lasntg\Admin\Group\GroupUtils;
+use Groups_Post_Access;
+
 class PrivatePaymentUtils {
 
 	public static function init() {
@@ -9,20 +12,18 @@ class PrivatePaymentUtils {
 	}
 
 	public static function add_filters() {
-		add_filter( 'woocommerce_available_payment_gateways', [ self::class, 'payment_gateway_disable_grant_funded' ] );
+		add_filter( 'woocommerce_available_payment_gateways', [ self::class, 'filter_available_payment_gateways' ] );
 	}
 
-	public static function payment_gateway_disable_grant_funded( $available_gateways ) {
-		if ( ! is_user_logged_in() || is_admin() ) {
+	public static function filter_available_payment_gateways( $available_gateways ) {
+
+		// admin orders.
+		if ( is_admin() || ( isset( $_POST['post_type'] ) && 'shop_order' === $_POST['post_type'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing
 			return $available_gateways;
 		}
-		$id = 'globalpayments_gpapi';
-		foreach ( $available_gateways as $key => $available_gateway ) {
-			if ( $id !== $key ) {
-				unset( $available_gateways[ $key ] );
-			}
-		}
 
-		return $available_gateways;
+		// shop orders.
+		$allowed_gateway_ids = [ 'globalpayments_gpapi' ];
+		return array_filter( $available_gateways, fn( $gateway_id ) => in_array( $gateway_id, $allowed_gateway_ids ), ARRAY_FILTER_USE_KEY );
 	}
 }
