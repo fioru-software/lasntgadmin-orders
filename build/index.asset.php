3d20d9c5aa21fa7150764282e128dbb5b1bf2b3b
cm <7zh6dj9f9@mozmail.com>
2023-05-16 12:09:55 +0100

    no group means available to all

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-url'), 'version' => '3edfaa507a9eb13541a9');
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-url'), 'version' => '69a2c6bf7ade83f2cf43');
