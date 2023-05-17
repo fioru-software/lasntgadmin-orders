dd4a5db470538cb0ab10c5e14adad1f341710138
cm <7zh6dj9f9@mozmail.com>
2023-05-17 11:00:29 +0100

    fresh build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-url'), 'version' => 'bb8c9f10fe5960119439');
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-url'), 'version' => '3d86b40f1b59c5163601');
