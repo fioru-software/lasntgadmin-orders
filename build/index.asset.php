07efde9c13c076127561c6c20ef741531d8d3ef2
cm <7zh6dj9f9@mozmail.com>
2023-05-18 14:08:01 +0100

    admin order rebuild

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-url'), 'version' => '3d86b40f1b59c5163601');
-=======
-<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-url'), 'version' => 'bb8c9f10fe5960119439');
->>>>>>> 436e236 (adding filters)
-=======
-<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-url'), 'version' => '3d86b40f1b59c5163601');
->>>>>>> dd4a5db (fresh build)
