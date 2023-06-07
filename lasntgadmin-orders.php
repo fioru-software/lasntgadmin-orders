fac1ed68f4f54533957ca41bfb113e298ea98a4c
Eric <emuigai5@gmail.com>
2023-06-07 13:47:12 +0300

    small updates

--- lasntgadmin-orders.php
+++ lasntgadmin-orders.php
@@ -3,7 +3,7 @@
  * Plugin Name:       LASNTG Orders
  * Plugin URI:        https://github.com/fioru-software/lasntgadmin-orders
  * Description:       Orders plugin
- * Version:           1.7.3
+ * Version:           1.7.4
  * Requires PHP:      7.2
  * Text Domain:       lasntgadmin
  * Domain Path:       /languages
