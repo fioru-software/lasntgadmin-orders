2bdae76af6c0ffdbbc5ec13f4e6e9fb256f7b83e
Charles Mulder <7zh6dj9f9@mozmail.com>
2023-06-22 20:43:45 +0100

    Attendee course prereqs met per course (#26)
    * docker compose update
    
    wip
    
    attendee course prereqs per course
    
    remove logging
    
    * attendee course prereqs met per course
    
    * attendee course prereqs met per course

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'dfe4aa125012c5be79db');
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '1cfe9ab5f08b678bd125');
