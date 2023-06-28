b775d6ebeb1b1148e76bfcc89ed775a479509a9b
cm <7zh6dj9f9@mozmail.com>
2023-06-28 11:25:32 +0100

    updated js

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '822c4da7b9cc8b6493b2');
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '58895886a660b63c2d12');
