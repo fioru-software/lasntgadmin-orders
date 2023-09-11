f366bd45001a35d9fc157242bebd4df7e7758407
cm <7zh6dj9f9@mozmail.com>
2023-09-11 12:40:32 +0100

    select

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '14dde7bb081d9821b9c6');
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '3f3c5b59f37b40bd7b44');
