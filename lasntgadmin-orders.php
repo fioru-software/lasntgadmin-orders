b77ff0ab345af0d4a739d1dfb2b1d76ef734afd4
cm <7zh6dj9f9@mozmail.com>
2024-01-23 14:08:25 +0000

    rc release

--- lasntgadmin-orders.php
+++ lasntgadmin-orders.php
@@ -3,7 +3,7 @@
  * Plugin Name:       LASNTG Orders
  * Plugin URI:        https://github.com/fioru-software/lasntgadmin-orders
  * Description:       Orders plugin
- * Version:           1.7.38
+ * Version:           1.7.39-rc1
  * Requires PHP:      7.2
  * Text Domain:       lasntgadmin
  * Domain Path:       /languages
