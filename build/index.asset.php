edb384db1f3cb58bfa270298309fb18a7194f4bb
cm <7zh6dj9f9@mozmail.com>
2024-02-22 15:32:41 +0000

    updated deps

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '4efc00b58850ebc8e4d9');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'e965a0b0a7f39359fcb8');
