5ade6b2f513a8c6fe6877d883f46170cda8061db
cm <7zh6dj9f9@mozmail.com>
2024-04-26 18:19:06 +0100

    wip

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'e965a0b0a7f39359fcb8');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'e0b8f2d33b222c7080cf');
