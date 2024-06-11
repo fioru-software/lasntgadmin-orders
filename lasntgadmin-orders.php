fe9b9c5c26423c63d0dc1ea6a009b60624689f92
Charles Mulder <7zh6dj9f9@mozmail.com>
2024-06-11 14:37:06 +0100

    improve order notifications (#69)
    * improve order notifications
    
    * stable release

--- lasntgadmin-orders.php
+++ lasntgadmin-orders.php
@@ -3,7 +3,7 @@
  * Plugin Name:       LASNTG Orders
  * Plugin URI:        https://github.com/fioru-software/lasntgadmin-orders
  * Description:       Orders plugin
- * Version:           1.8.0
+ * Version:           1.8.1
  * Requires PHP:      7.2
  * Text Domain:       lasntgadmin
  * Domain Path:       /languages
