bf12438aa1c90ad74817ba64b7dc69714001e2d3
cm <7zh6dj9f9@mozmail.com>
2024-07-24 15:33:44 +0100

    parking

--- lib/PaymentGatewayActionsFilters.php
+++ lib/PaymentGatewayActionsFilters.php
@@ -18,31 +18,35 @@ class PaymentGatewayActionsFilters {
 
 	public static function filter_available_payment_gateways( array $available_gateways ): array {
 
-		error_log("==== filter available payment gateways ===");
+		$allowed_gateway_ids = $available_gateways;
+
 		// admin orders.
 		if ( is_admin() || ( isset( $_POST['post_type'] ) && 'shop_order' === $_POST['post_type'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing
-			return $available_gateways;
+			$allowed_gateway_ids = [ 'lasntgadmin_grant_funded_payment_gateway', 'woocommerce_gateway_purchase_order' ];
 		}
 
-		if( is_checkout() ) {
+		// shop orders.
+		if( ! is_admin() ) {
 			$cart = WC()->cart;
 			if( ! $cart->is_empty() ) {
 				$cart_item_count = $cart->get_cart_contents_count();
+
+				/**
+				 * Only allow ordering a single course at a time.
+				 */
 				if( $cart_item_count > 1 ) {
 					$cart->empty_cart();
-					wp_safe_redirect( wc_get_page_permalink( 'shop' ) );
-					return [];
+					wc_add_notice("Only a single course per order is supported.", "error");
+					wp_safe_redirect( wc_get_checkout_url() );
+					exit();
 				}
 				$cart_items = $cart->get_cart();
 				$cart_item = current( $cart_items );
 				$product_id = $cart_item['product_id'];
 				$training_centre_id = get_field('training_centre', $product_id, false, false );
+				$allowed_gateway_ids = [ 'realex_redirect', 'cod' ];
 			}
 		}
-
-		// shop orders.
-		// lasntgadmin_grant_funded_payment_gateway, woocommerce_gateway_purchase_order
-		$allowed_gateway_ids = [ 'realex_redirect', 'cod' ];
 
 		return array_filter( $available_gateways, fn( $gateway_id ) => in_array( $gateway_id, $allowed_gateway_ids ), ARRAY_FILTER_USE_KEY );
 	}
