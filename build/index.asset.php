44e8d673d84106ae8a520bd726e6ab11dc85162f
Charles Mulder <7zh6dj9f9@mozmail.com>
2025-01-15 13:42:48 +0000

    node version upgrade (#81)
    * node version upgrade
    
    * stable release

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'f39dfb42b272ba14d665');
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '1d2b08df49b0051745fb');
