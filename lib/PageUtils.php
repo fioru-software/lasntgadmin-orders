dcf7ab6c7f098b6d3344abc91a61fc47981ed866
cm <7zh6dj9f9@mozmail.com>
2025-03-06 16:24:36 +0000

    remove unused scripts from orders

--- lib/PageUtils.php
+++ lib/PageUtils.php
@@ -84,7 +84,7 @@ class PageUtils {
 	public static function remove_unused_scripts() {
 		if ( function_exists( 'get_post_type' ) ) {
 			if ( 'shop_order' === get_post_type() ) {
-				$handles = [ 'wp-mediaelement', 'thickbox', 'woocommerce-order-attribution-admin-js', 'wc-admin-order-meta-boxes', 'image-edit', 'marketplace-suggestions', 'shortcode', 'quicktags' ];
+				$handles = [ 'wp-mediaelement', 'thickbox', 'woocommerce-order-attribution-admin-js', 'wc-admin-order-meta-boxes', 'image-edit', 'marketplace-suggestions', 'shortcode', 'quicktags', 'media-audiovideo', 'mce-view', 'media-editor' ];
 				foreach ( $handles as $handle ) {
 					wp_dequeue_script( $handle );
 					wp_deregister_script( $handle );
