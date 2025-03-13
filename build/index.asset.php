a06716af5e075e75e6f9ff9979eecfeb8093f0fb
Charles Mulder <7zh6dj9f9@mozmail.com>
2025-03-13 17:20:54 +0000

    fix when removing empty attendee (#85)
    * fix when removing empty attendee
    
    * stable release

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '5e08192fcec6cad6a538');
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '719d67f1b4c74439e455');
